e241cb20fbfd25298f0034d47803d18247c492b9
baiyidjp <tztddong@dongjiangpengdemac-mini.local>
2016-07-06 10:52:46 +0800

    Initial Commit

---
+++ Masonry-Djp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Masonry-Djp
+//
+//  Created by tztddong on 16/7/6.
+//  Copyright © 2016年 dongjiangpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
